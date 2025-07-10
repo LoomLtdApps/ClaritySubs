38e18cfd52dad57b423938f2e1a9d71770417313
LoomLtdApps <florian.schelling@icloud.com>
2025-07-10 11:26:29 +0800

    Swipr Gestures

---
+++ MailSwipeActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailSwipeActionsApp.swift
+//  MailSwipeActions
+//
+//  Created by Balaji Venkatesh on 04/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MailSwipeActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
