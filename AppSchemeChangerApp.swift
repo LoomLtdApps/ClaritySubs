57a4dc40106febe8a7fc040e214b3e18e2b4f6ec
LoomLtdApps <florian.schelling@icloud.com>
2025-07-10 11:26:55 +0800

    APpSheme Switcher

---
+++ AppSchemeChangerApp.swift
@@ -0,0 +1,19 @@
+//
+//  AppSchemeChangerApp.swift
+//  AppSchemeChanger
+//
+//  Created by Balaji Venkatesh on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AppSchemeChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SchemeHostView {
+                ContentView()
+            }
+        }
+    }
+}
